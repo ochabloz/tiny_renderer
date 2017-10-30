962f026537d97f8b0b66a4feef0a44808306811f
Olivier Chabloz <olivier.chabloz@sensima.ch>
2017-10-30 17:35:22 +0100

    update of tests

--- tests/tests_tiny_renderer.c
+++ tests/tests_tiny_renderer.c
@@ -51,7 +51,7 @@ TEST(renderer, draw_a_triangle){
     vec2i_t t0[3] = {vec2i_create(10, 70),   vec2i_create(50, 160),  vec2i_create(70, 80)};
     vec2i_t t1[3] = {vec2i_create(180, 50),  vec2i_create(150, 1),   vec2i_create(70, 180)};
     vec2i_t t2[3] = {vec2i_create(180, 150), vec2i_create(120, 160), vec2i_create(130, 180)};
-    vec2i_t t3[3] = {vec2i_create(10, 60),   vec2i_create(10, 1),  vec2i_create(50, 60)};
+    vec2i_t t3[3] = {vec2i_create(20, 60),   vec2i_create(20, 1),  vec2i_create(60, 60)};
     draw_triangle(f, t0, 0xFF0000);
     draw_triangle(f, t1, 0xFFFFFF);
     draw_triangle(f, t2, 0x00FF00);
@@ -65,6 +65,6 @@ TEST(renderer, draw_a_triangle){
 TEST(renderer, test_render_obj){
     bmp_file_t ren = bmp_file_create(1200,1200);
     render_obj(ren, "objects/african_head.obj", 1200, 1200);
-    bmp_file_save(ren, "diablo_renderer.bmp");
+    bmp_file_save(ren, "african_head.bmp");
     bmp_file_delete(ren);
 }
